50773dc2c4fea1268f408dcc665cf623c3eb0982
projases <140944318+projases@users.noreply.github.com>
2024-03-21 09:20:50 +0100

    changed path

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,7 +5,7 @@ import { searchbar } from "../searchbar/searchbar";
 export function navbar() {
   const navbar = `
     <ul class="navbar">
-      <img src="/Users/pablorojas/Documents/GitHub/Frontend/javascript/Painterest/src/assets/painterest.png" alt="Painterest Logo">
+      <img src="../../assets/painterest.png" alt="Painterest Logo">
       ${button({ text: "Home", color: "dark" })}
       ${button({ text: "Create", color: "light" })}
       ${button({ text: "Go", color: "light" })}
