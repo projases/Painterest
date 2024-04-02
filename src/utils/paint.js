1781386fcd3ca8d8f5f3659ce3ec7dc0d8b15494
projases <140944318+projases@users.noreply.github.com>
2024-04-02 16:27:35 +0200

    responsive change

--- src/utils/paint.js
+++ src/utils/paint.js
@@ -1,19 +1,23 @@
 export const paint = (data) => {
   const gallery = document.querySelector(".gallery");
   gallery.innerHTML = "";
-  data.forEach((photo, index) => {
-    const img = document.createElement("img");
-    img.src = photo.urls.regular;
-    img.alt = photo.alt_description;
+  if (data.length === 0) {
+    gallery.innerHTML = `<p class="emptySearch">No results found</p>`;
+  } else {
+    data.forEach((photo, index) => {
+      const img = document.createElement("img");
+      img.src = photo.urls.regular;
+      img.alt = photo.alt_description;
 
-    gallery.append(img);
+      gallery.append(img);
 
-    if ((index + 1) % 3 === 0) {
-      img.classList.add("large");
-    } else if ((index + 1) % 2 === 0) {
-      img.classList.add("medium");
-    } else {
-      img.classList.add("small");
-    }
-  });
+      if ((index + 1) % 3 === 0) {
+        img.classList.add("large");
+      } else if ((index + 1) % 2 === 0) {
+        img.classList.add("medium");
+      } else {
+        img.classList.add("small");
+      }
+    });
+  }
 };
